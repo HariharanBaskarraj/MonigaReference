3f84164dceb34a7112ed838dd0058d79de352acc
Hariharan B <hariharan.b@acheron-tech.com>
2023-10-08 22:13:03 +0530

    Initial Commit

---
+++ PojoDemo/src/com/moniga/sample/InheritanceCheck.java
@@ -0,0 +1,12 @@
+package com.moniga.sample;
+
+import com.moniga.pojo.Employee;
+
+public class InheritanceCheck extends Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
