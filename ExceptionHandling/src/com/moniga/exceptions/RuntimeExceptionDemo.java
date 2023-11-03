63ee360f320c30c3f158e4d4c31e678d64f54e90
Hariharan B <hariharan.b@acheron-tech.com>
2023-11-04 00:59:39 +0530

    Exception Day 01 Commit

---
+++ ExceptionHandling/src/com/moniga/exceptions/RuntimeExceptionDemo.java
@@ -0,0 +1,47 @@
+package com.moniga.exceptions;
+
+import java.util.Scanner;
+
+public class RuntimeExceptionDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter first number : ");
+		int numberOne = sc.nextInt();
+//		sc.nextLine();
+
+		System.out.println("Enter second number : ");
+		int numberTwo = sc.nextInt();
+		sc.nextLine();
+
+//		normalDivision(numberOne, numberTwo);
+		tryDivision(numberOne, numberTwo);
+		
+		sc.close();
+
+	}
+
+	public static void normalDivision(int numberOne, int numberTwo) {
+		if (numberTwo == 0) {
+			System.out.println("/ by zero is not possible");
+		} else {
+			int divisionResult = numberOne / numberTwo;
+			System.out.println("Division Result : " + divisionResult);
+		}
+	}
+
+	public static void tryDivision(int numberOne, int numberTwo) {
+		try { // error prone statements
+			int divisionResult = numberOne / numberTwo;
+			System.out.println(divisionResult);
+			
+
+System.out.println(Integer.parseInt("Moniga"));
+		} catch (ArithmeticException e) { // handling code
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+	}
+}
