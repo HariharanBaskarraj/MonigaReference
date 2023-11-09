eabed03a4f08bfff50934799e5a43d4f6155a8a7
Hariharan B <hariharan.b@acheron-tech.com>
2023-11-09 08:27:48 +0530

    Static Commit

---
+++ Static/src/com/moniga/staticdemo/twoclass/DoctorMain.java
@@ -0,0 +1,12 @@
+package com.moniga.staticdemo.twoclass;
+
+public class DoctorMain {
+
+	public static void main(String[] args) {
+		Doctor doctor = new Doctor("Moniga", "Cardiologist");
+		System.out.println(doctor.getName() + " " + doctor.getSpeciality() + " " + Doctor.getExperience());
+		// Static way of calling methods :
+		// ClassName.methodName();
+	}
+
+}
